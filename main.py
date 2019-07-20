712607650d560c819c98008e39bfa9f39311d4e4
MoriKamiya <MoriKamiya@users.noreply.github.com>
2019-07-21 00:58:00 +0800

    V 0.0.2
    追加辨識 離開組隊房間

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-"""
-AAA 進入組隊房間
-BBB 進入組隊房間
-CCC 進入組隊房間
-DDD 進入組隊房間
-EEE 進入組隊房間
-"""
 import webbrowser
 
 ID_Mem = []
+error_Mem = []
 ID_Query = ""
 temp = ""
 LOG_URL = "https://www.leagueofgraphs.com/summoner/tw/"
@@ -16,11 +10,13 @@ while 1:
     while original_Str != "":
         if original_Str[len(original_Str)-6 : len(original_Str)] == "進入組隊房間":
             temp = original_Str.replace("進入組隊房間","").strip()
+        elif original_Str[len(original_Str)-6 : len(original_Str)] == "離開組隊房間":
+            temp = original_Str.replace("離開組隊房間","").strip()    
         elif ":  " in original_Str: 
             temp = original_Str.split(":", 1)
             temp = temp[0].strip()
         else:
-            print("Error content format!:\r\n" + original_Str)
+            error_Mem.append(original_Str)
         if temp in ID_Mem:
             """NOP"""
         else:
@@ -28,11 +24,22 @@ while 1:
                 ID_Mem.append(temp)
                 temp = ""
         original_Str = input()
+    for i in range(0,len(error_Mem)):
+        error_String = error_Mem.pop()
+        print("Error content format!:\r\n" + error_String)
     while len(ID_Mem)!= 0:
         if len(ID_Mem) == 0:
             break
         ID_Query = ID_Mem.pop()
         webbrowser.open(LOG_URL + ID_Query, new = 0)
+
+"""
+AAA 進入組隊房間
+BBB 進入組隊房間
+CCC 進入組隊房間
+DDD 進入組隊房間
+EEE 進入組隊房間
+"""
         
 
 
